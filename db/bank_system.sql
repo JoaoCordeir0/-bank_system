d6c0a566e12e58c1a14435279a6e5da3660e477b
joao.cordeiro <joaocordeiro2134@gmail.com>
2023-08-14 12:12:10 -0300

    add bk database

--- db/bank_system.sql
+++ db/bank_system.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `bank_system` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
+USE `bank_system`;
 -- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: bank_system
@@ -14,6 +16,60 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `balance`
+--
+
+DROP TABLE IF EXISTS `balance`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `balance` (
+  `fk_user_id` int(11) DEFAULT NULL,
+  `user_balance` float NOT NULL,
+  KEY `FK_balance_user` (`fk_user_id`),
+  CONSTRAINT `FK_balance_user` FOREIGN KEY (`fk_user_id`) REFERENCES `users` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `balance`
+--
+
+LOCK TABLES `balance` WRITE;
+/*!40000 ALTER TABLE `balance` DISABLE KEYS */;
+INSERT INTO `balance` VALUES (1,1400);
+/*!40000 ALTER TABLE `balance` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `log`
+--
+
+DROP TABLE IF EXISTS `log`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `log` (
+  `log_id` int(11) NOT NULL AUTO_INCREMENT,
+  `fk_user_id` int(11) NOT NULL,
+  `log_action` varchar(50) NOT NULL,
+  `log_description` text NOT NULL,
+  `log_date_insert` datetime NOT NULL,
+  PRIMARY KEY (`log_id`),
+  KEY `FK_log_user` (`fk_user_id`),
+  CONSTRAINT `FK_log_user` FOREIGN KEY (`fk_user_id`) REFERENCES `users` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `log`
+--
+
+LOCK TABLES `log` WRITE;
+/*!40000 ALTER TABLE `log` DISABLE KEYS */;
+INSERT INTO `log` VALUES (1,1,'deposit | 1','Deposit of 500,00','2023-08-13 19:34:13'),(2,1,'deposit | 1','Deposit of 2400,00','2023-08-13 19:34:57'),(3,1,'withdraw | 1','Withdraw of 1328,00','2023-08-13 19:35:08'),(5,1,'withdraw','Withdraw of 123,00','2023-08-14 08:13:57'),(6,1,'withdraw','Withdraw of 49,00','2023-08-14 08:22:31');
+/*!40000 ALTER TABLE `log` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `users`
@@ -51,4 +107,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-08-13 19:41:50
+-- Dump completed on 2023-08-14 12:09:38
